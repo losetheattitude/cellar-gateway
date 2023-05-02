1789eab4091a451bc45433ffc6b6e69d31188b3e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2023-05-02 04:41:21 +0300

    [PRUEBA]

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, {Request,Response,Application} from 'express';
 const app:Application = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req:Request, res:Response):void => {
     res.send("Hello Typescript with Node.js!");
